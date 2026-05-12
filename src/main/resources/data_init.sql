4e8357fb4bbac98997204b43992ae3dc96efc265
BillerPlay <abulyaa@gmail.com>
2026-05-12 16:49:43 +0400

    Added data initialization file

---
+++ src/main/resources/data_init.sql
@@ -0,0 +1,6 @@
+INSERT INTO quote (author, text, category, created_at) VALUES
+                                                           ('Marcus Aurelius', 'The impediment to action advances action. What stands in the way becomes the way.', 'philosophy', NOW()),
+                                                           ('Seneca', 'We suffer more in imagination than in reality.', 'stoicism'),
+                                                           ('Friedrich Nietzsche', 'That which does not kill us makes us stronger.', 'philosophy', NOW()),
+                                                           ('Maya Angelou', 'You will face many defeats in life, but never let yourself be defeated.', 'motivation', NOW()),
+                                                           ('Albert Einstein', 'A person who never made a mistake never tried anything new.', 'science', NOW());
